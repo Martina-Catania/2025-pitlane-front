6cccfe912d36d5d234d141ad552cff065c6339f7
TheKypers <ignaciogmillan@gmail.com>
2025-09-26 21:38:43 -0300

    deployment fixes

--- quecomemos-app/middleware.ts
+++ quecomemos-app/middleware.ts
@@ -1,5 +1,8 @@
-import { updateSession } from "@/lib/supabase/middleware";
+import { updateSession } from "./lib/supabase/middleware";
 import { type NextRequest } from "next/server";
+
+// Specify Edge Runtime
+export const runtime = 'edge';
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
