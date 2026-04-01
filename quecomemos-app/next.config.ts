48661b2d1720b00983debfbdd5fddc7f7246b631
Martina-Catania <tinicatania1111@gmail.com>
2026-03-31 21:29:14 -0300

    update

--- quecomemos-app/next.config.ts
+++ quecomemos-app/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'hcakhjdklvxacqdcexuv.supabase.co',
+        hostname: 'uwzrnfxbkoeltqzoirks.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
